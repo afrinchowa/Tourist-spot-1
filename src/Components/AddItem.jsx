442678a919b3e2947cd77fe85ad681ee417924d5
Chowa <arfinchowa524@gmail.com>
2024-07-04 13:34:16 +0600

    form works

--- src/Components/AddItem.jsx
+++ src/Components/AddItem.jsx
@@ -1,11 +1,35 @@
-
-
 const AddItem = () => {
-    return (
-        <div>
-            AddItem
-        </div>
-    );
+  return (
+    <div>
+      Add spot
+      <form>
+       <div>
+		<div className="flex">
+		<label className="form-control w-full max-w-xs">
+          <div className="label">
+            <span className="label-text">What is your name?</span>
+          </div>
+          <input
+            type="text"
+            placeholder="Type here"
+            className="input input-bordered w-full max-w-xs"
+          />
+        </label>
+		<label className="form-control w-full max-w-xs">
+          <div className="label">
+            <span className="label-text">What is your name?</span>
+          </div>
+          <input
+            type="text"
+            placeholder="Type here"
+            className="input input-bordered w-full max-w-xs"
+          />
+        </label>
+		</div>
+	   </div>
+      </form>
+    </div>
+  );
 };
 
 export default AddItem;

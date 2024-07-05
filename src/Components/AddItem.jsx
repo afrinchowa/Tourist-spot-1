e7f1248906fcdb36c319139d036a87d483918f94
Chowa <arfinchowa524@gmail.com>
2024-07-05 21:10:59 +0600

    Create Add spot

--- src/Components/AddItem.jsx
+++ src/Components/AddItem.jsx
@@ -1,32 +1,109 @@
 const AddItem = () => {
   return (
-    <div>
-      Add spot
+    <div className="bg-[#C3D898] text-[#70161E] p-24">
+      <h2 className="text-3xl font-extrabold">Add spot</h2>
       <form>
-       <div>
-		<div className="flex">
-		<label className="form-control w-full max-w-xs">
-          <div className="label">
-            <span className="label-text">What is your name?</span>
+        <div>
+          <div className="flex">
+            <label className="form-control md:w-1/2 mr-4">
+              <div className="label">
+                <span className="label-text">Tourists_spot_name</span>
+              </div>
+              <input
+                type="text"
+                placeholder="Tourists_spot_name"
+                name="spotName"
+                className="input input-bordered w-full "
+              />
+            </label>
+            <label className="form-control md:w-1/2  ">
+              <div className="label">
+                <span className="label-text">Country_Name</span>
+              </div>
+              <input
+                type="text"
+                name="countryName"
+                placeholder="Country_Name"
+                className="input input-bordered w-full "
+              />
+            </label>
           </div>
-          <input
-            type="text"
-            placeholder="Type here"
-            className="input input-bordered w-full max-w-xs"
-          />
-        </label>
-		<label className="form-control w-full max-w-xs">
-          <div className="label">
-            <span className="label-text">What is your name?</span>
+          <div className="flex">
+            <label className="form-control w-1/2 mr-4">
+              <div className="label">
+                <span className="label-text">Location</span>
+              </div>
+              <input
+                type="text"
+                name="location"
+                placeholder="Location"
+                className="input input-bordered w-full "
+              />
+            </label>
+            <label className="form-control w-1/2">
+              <div className="label">
+                <span className="label-text">Short description</span>
+              </div>
+              <input
+                type="text"
+                name="shortDescription"
+                placeholder="Short description"
+                className="input input-bordered w-full "
+              />
+            </label>
           </div>
-          <input
-            type="text"
-            placeholder="Type here"
-            className="input input-bordered w-full max-w-xs"
-          />
-        </label>
-		</div>
-	   </div>
+          <div className="flex">
+            <label className="form-control md:w-1/2 mr-4">
+              <div className="label">
+                <span className="label-text">Average_cost</span>
+              </div>
+              <input
+                type="text"
+                placeholder="Average_cost
+"name="averageCost"
+                className="input input-bordered w-full "
+              />
+            </label>
+            <label className="form-control md:w-1/2">
+              <div className="label">
+                <span className="label-text">Seasonality</span>
+              </div>
+              <input
+                type="text"
+                placeholder="Seasonality"
+                name="seasonality"
+                className="input input-bordered w-full "
+              />
+            </label>
+          </div>
+          <div className="flex mb-6">
+            <label className="form-control md:w-1/2 mr-4">
+              <div className="label">
+                <span className="label-text">Image URL</span>
+              </div>
+              <input
+                type="text"
+                placeholder="Image URL
+" name="img"
+                className="input input-bordered w-full "
+              />
+            </label>
+            <label className="form-control md:w-1/2">
+              <div className="label">
+                <span className="label-text">Travel_time</span>
+              </div>
+              <input
+                type="text"
+                placeholder="Travel_time"
+                name="travelTime"
+                className="input input-bordered w-full "
+              />
+            </label>
+          </div>
+
+          
+          <input type="submit" value="add Spot" className="btn btn-block" />
+        </div>
       </form>
     </div>
   );

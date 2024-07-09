043b8f78c596a8703a3778443f44b75138764302
Chowa <arfinchowa524@gmail.com>
2024-07-09 23:41:34 +0600

    new carusal uploaded

--- src/App.jsx
+++ src/App.jsx
@@ -7,35 +7,27 @@ function App() {
     <>
       
       <h1 className="text-6xl text-red-600">Vite + React</h1>
-      <div className="carousel carousel-end rounded-box">
-  <div className="carousel-item">
-    <img src="https://img.daisyui.com/images/stock/photo-1559703248-dcaaec9fab78.jpg" alt="Drink" />
+      <div className="carousel carousel-vertical rounded-box h-96">
+  <div className="carousel-item h-full">
+    <img src="https://img.daisyui.com/images/stock/photo-1559703248-dcaaec9fab78.jpg" />
   </div>
-  <div className="carousel-item">
-    <img
-      src="https://img.daisyui.com/images/stock/photo-1565098772267-60af42b81ef2.jpg"
-      alt="Drink" />
+  <div className="carousel-item h-full">
+    <img src="https://img.daisyui.com/images/stock/photo-1565098772267-60af42b81ef2.jpg" />
   </div>
-  <div className="carousel-item">
-    <img
-      src="https://img.daisyui.com/images/stock/photo-1572635148818-ef6fd45eb394.jpg"
-      alt="Drink" />
+  <div className="carousel-item h-full">
+    <img src="https://img.daisyui.com/images/stock/photo-1572635148818-ef6fd45eb394.jpg" />
   </div>
-  <div className="carousel-item">
-    <img
-      src="https://img.daisyui.com/images/stock/photo-1494253109108-2e30c049369b.jpg"
-      alt="Drink" />
+  <div className="carousel-item h-full">
+    <img src="https://img.daisyui.com/images/stock/photo-1494253109108-2e30c049369b.jpg" />
   </div>
-  <div className="carousel-item">
-    <img src="https://img.daisyui.com/images/stock/photo-1550258987-190a2d41a8ba.jpg" alt="Drink" />
+  <div className="carousel-item h-full">
+    <img src="https://img.daisyui.com/images/stock/photo-1550258987-190a2d41a8ba.jpg" />
   </div>
-  <div className="carousel-item">
-    <img src="https://img.daisyui.com/images/stock/photo-1559181567-c3190ca9959b.jpg" alt="Drink" />
+  <div className="carousel-item h-full">
+    <img src="https://img.daisyui.com/images/stock/photo-1559181567-c3190ca9959b.jpg" />
   </div>
-  <div className="carousel-item">
-    <img
-      src="https://img.daisyui.com/images/stock/photo-1601004890684-d8cbf643f5f2.jpg"
-      alt="Drink" />
+  <div className="carousel-item h-full">
+    <img src="https://img.daisyui.com/images/stock/photo-1601004890684-d8cbf643f5f2.jpg" />
   </div>
 </div>
     </>

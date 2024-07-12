041e7c7cf490914a3eb83037fe084ac940c2e4b0
Chowa <arfinchowa524@gmail.com>
2024-07-12 23:39:22 +0600

    create card

--- src/App.jsx
+++ src/App.jsx
@@ -21,6 +21,32 @@ const spots = useLoaderData();
     <img src="https://img.daisyui.com/images/stock/photo-1601004890684-d8cbf643f5f2.jpg" />
   </div>
 </div>
+
+<div>
+<div className="card card-side bg-base-100 shadow-xl">
+  <figure>
+    <img
+      src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.jpg"
+      alt="Movie" />
+  </figure>
+  <div className="card-body">
+    <h2 className="card-title">New movie is released!</h2>
+    <p>Click the button to watch on Jetflix app.</p>
+    <div className="card-actions justify-end">
+      <button className="btn btn-primary">Watch</button>
+    </div>
+  </div>
+</div>
+</div>
+
+
+
+
+
+
+
+
+
     </>
   )
 }

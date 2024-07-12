5e996b7792d41730b37df784accc5ff1fece775a
Chowa <arfinchowa524@gmail.com>
2024-07-13 00:13:18 +0600

    btn set up 1

--- src/Components/SportCard.jsx
+++ src/Components/SportCard.jsx
@@ -9,9 +9,14 @@ const {spotName,countryName,location,shortDescription,averageCost,seasonality,im
         <img
           src={img}/>
       </figure>
-      <div className="card-body">
+      <div className="flex justify-between w-full pr-4">
+        <div>
         <h2 className="card-title">Spot-Name: {spotName}</h2>
         <p>Country Name: {countryName}</p>
+        <p>Country Name: {location}</p>
+        <p>Country Name: {shortDescription}</p>
+        <p>Country Name: {averageCost}</p>
+        </div>
         <div className="card-actions justify-end">
          <div className="">
 

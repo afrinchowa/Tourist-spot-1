6e00ed0bc3d0615396ccca44d472974b29530c2d
Chowa <arfinchowa524@gmail.com>
2024-07-12 23:47:43 +0600

    create card v2

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,8 @@ function App() {
 
   return (
     <>
-      <h1 className="text-6xl text-red-600">Hottest Spot:{spots.length}</h1>
+    <div className="m-20 ">
+      <h1 className="text-6xl text-center text-red-600">Hottest Spot:{spots.length}</h1>
       <div className="carousel carousel-vertical rounded-box h-96">
         <div className="carousel-item h-full">
           <img src="https://img.daisyui.com/images/stock/photo-1550258987-190a2d41a8ba.jpg" />
@@ -19,10 +20,12 @@ function App() {
         </div>
       </div>
 
+<div className=" grid md:grid-cols-2 gap-4">
 {
   spots.map(spot =>      <SportCard key={spot._id} spot={spot}></SportCard> )
 }
-
+</div>
+</div>
     </>
   );
 }

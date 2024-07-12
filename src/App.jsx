1960aca80d57e41b55581455c2dc2f685515bfb9
Chowa <arfinchowa524@gmail.com>
2024-07-12 23:43:15 +0600

    create card v1

--- src/App.jsx
+++ src/App.jsx
@@ -1,54 +1,30 @@
-import { useLoaderData } from "react-router-dom"
-
-
+import { useLoaderData } from "react-router-dom";
+import SportCard from "./Components/SportCard";
 
 function App() {
- 
-const spots = useLoaderData();
+  const spots = useLoaderData();
 
   return (
     <>
       <h1 className="text-6xl text-red-600">Hottest Spot:{spots.length}</h1>
       <div className="carousel carousel-vertical rounded-box h-96">
-  
-  <div className="carousel-item h-full">
-    <img src="https://img.daisyui.com/images/stock/photo-1550258987-190a2d41a8ba.jpg" />
-  </div>
-  <div className="carousel-item h-full">
-    <img src="https://img.daisyui.com/images/stock/photo-1559181567-c3190ca9959b.jpg" />
-  </div>
-  <div className="carousel-item h-full">
-    <img src="https://img.daisyui.com/images/stock/photo-1601004890684-d8cbf643f5f2.jpg" />
-  </div>
-</div>
+        <div className="carousel-item h-full">
+          <img src="https://img.daisyui.com/images/stock/photo-1550258987-190a2d41a8ba.jpg" />
+        </div>
+        <div className="carousel-item h-full">
+          <img src="https://img.daisyui.com/images/stock/photo-1559181567-c3190ca9959b.jpg" />
+        </div>
+        <div className="carousel-item h-full">
+          <img src="https://img.daisyui.com/images/stock/photo-1601004890684-d8cbf643f5f2.jpg" />
+        </div>
+      </div>
 
-<div>
-<div className="card card-side bg-base-100 shadow-xl">
-  <figure>
-    <img
-      src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.jpg"
-      alt="Movie" />
-  </figure>
-  <div className="card-body">
-    <h2 className="card-title">New movie is released!</h2>
-    <p>Click the button to watch on Jetflix app.</p>
-    <div className="card-actions justify-end">
-      <button className="btn btn-primary">Watch</button>
-    </div>
-  </div>
-</div>
-</div>
-
-
-
-
-
-
-
-
-
-    </>
-  )
+{
+  spots.map(spot =>      <SportCard key={spot._id} spot={spot}></SportCard> )
 }
 
-export default App
+    </>
+  );
+}
+
+export default App;
--- src/Components/SportCard.jsx
+++ src/Components/SportCard.jsx
@@ -1,22 +1,21 @@
 
 
-const SportCard = () => {
+const SportCard = ({spot}) => {
     return (
-        <div className="card bg-base-100 w-96 shadow-xl">
-        <figure className="px-10 pt-10">
-          <img
-            src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
-            alt="Shoes"
-            className="rounded-xl" />
-        </figure>
-        <div className="card-body items-center text-center">
-          <h2 className="card-title"></h2>
-          <p> </p>
-          <div className="card-actions">
-            <button className="btn btn-primary">Buy Now</button>
-          </div>
+      <div className="card card-side bg-base-100 shadow-xl">
+      <figure>
+        <img
+          src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.jpg"
+          alt="Movie" />
+      </figure>
+      <div className="card-body">
+        <h2 className="card-title">New movie is released!</h2>
+        <p>Click the button to watch on Jetflix app.</p>
+        <div className="card-actions justify-end">
+          <button className="btn btn-primary">Watch</button>
         </div>
       </div>
+    </div>
     );
 };
 
